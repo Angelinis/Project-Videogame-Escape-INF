4f9a778cc1d66b92d00744a3e84eb84ef9b9eeae
Angelo <41450351+Angelinis@users.noreply.github.com>
2023-06-26 13:47:58 -0300

    Adding listener to the configure menu

--- Escape INF/Interface/ConfigureMenu/ConfigureMenu.gd
+++ Escape INF/Interface/ConfigureMenu/ConfigureMenu.gd
@@ -2,6 +2,8 @@ extends Panel
 
 onready var pause = false
 
+var selectedOption = 0
+
 const MAIN_MENU_PATH = "res://Interface/MainMenu/MainMenu.tscn"
 
 func _on_PauseMenuRect_gui_input(event):
@@ -28,64 +30,48 @@ func _input(event):
 			Blur.visible = false
 			get_tree().paused = false	
 			pause = false
+		elif event.pressed and event.scancode == KEY_DOWN and pause:
+			selectedOption += 1
+			adjustSelectedOption()
+		elif event.pressed and event.scancode == KEY_UP and pause:
+			selectedOption -= 1
+			adjustSelectedOption()
+		elif event.pressed and event.scancode == KEY_ENTER and pause:
+			handleSelectedOption()
+			
+			
+func adjustSelectedOption():
+	
+	for i in range($VBoxContainer.get_child_count()):
+		var button = $VBoxContainer.get_child(i)
+
+		if i == selectedOption:
+			button.add_color_override("font_color", Color(1, 1, 1))
+#			AudioPlayer.play_one_shot(SOUNDARRAY[i], "MenuSpeech") 
+		else:
+			button.add_color_override("font_color", Color(0.5, 0.5, 0.5))
+
+
+func handleSelectedOption():
+	match selectedOption:
+		0:
+			_on_Resume_pressed()
+		1:
+			print("Sound")
+		2:
+			print("Music")
+		3:	
+			print("MenuSound")
+		4:	
+			print("Dialogues")
+		5:	
+			print("UI Sound")
+		6:	
+			_on_MainMenuButton_pressed()
+
 
 func _on_MainMenuButton_pressed():
 	get_tree().paused = false
 	SceneTransition.transition_scene(MAIN_MENU_PATH)
-
-#var selectedButtonIndex = 0
-#
-#func _input(event):
-#	if event is InputEventKey and event.is_pressed():
-#		if event.scancode == KEY_DOWN:
-#			select_next_button()
-#		elif event.scancode == KEY_UP:
-#			select_previous_button()
-#		elif event.scancode == KEY_ENTER:
-#			select_current_button()
-#
-#func select_next_button():
-#	selectedButtonIndex += 1
-#	if selectedButtonIndex >= 3:
-#		selectedButtonIndex = 0
-#
-#	update_button_selection()
-#
-#func select_previous_button():
-#	selectedButtonIndex -= 1
-#	if selectedButtonIndex < 0:
-#		selectedButtonIndex = 2
-#
-#	update_button_selection()
-#
-#func select_current_button():
-#	match selectedButtonIndex:
-#		0:
-#			_on_Resume_pressed()
-#		1:
-#			print("Sound")
-#		2:
-#			print("Music")
-#		3:	
-#			print("MenuSpeech")
-#		4:
-#			_on_MainMenuButton_pressed()
-#
-#var SOUNDMENU1 = preload("res://Audio/AudioInclusive/MainMenu/main_menu_jogar_voice_text.mp3")
-#var SOUNDMENU2 = preload("res://Audio/AudioInclusive/MainMenu/main_menu_como_jogar_voice_text.mp3")
-#var SOUNDMENU3 = preload("res://Audio/AudioInclusive/MainMenu/main_menu_sair_voice_text.mp3")
-#var SOUNDMENU4 = preload("res://Audio/AudioInclusive/MainMenu/main_menu_continuar_voice_text.mp3")
-#var SOUNDARRAY = [SOUNDMENU1, SOUNDMENU2, SOUNDMENU3, SOUNDMENU4]
-#
-#
-#func update_button_selection():
-#	for i in range($Buttons.get_child_count()):
-#		var button = $Buttons.get_child(i)
-#
-#		if i == selectedButtonIndex:
-#			if i ==0 and $Buttons/PlayButton.text == "CONTINUAR":
-#				i = 3
-#			button.add_color_override("font_color", Color(1, 1, 1))
-#			AudioPlayer.play_one_shot(SOUNDARRAY[i], "MenuSpeech") 
-#		else:
-#			button.add_color_override("font_color", Color(0.5, 0.5, 0.5))
+	
+	
