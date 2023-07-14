1cb49e1e266fba002e771e9c7488ad863269137d
Angelo <41450351+Angelinis@users.noreply.github.com>
2023-07-14 18:37:08 -0300

    Adding TAB listener in WallsManager to toogle between readable objects.

--- Escape INF/Rooms/WallsManager.gd
+++ Escape INF/Rooms/WallsManager.gd
@@ -2,6 +2,14 @@ extends Node2D
 
 var current_wall_index := 0 setget set_current_wall
 
+var selected_scene
+
+var selected_hover_info
+
+var total_number_hover_info
+
+var index_hover_info := 0
+
 onready var walls := get_children()
 
 func _ready():
@@ -15,6 +23,22 @@ func _ready():
 	for wall in walls:
 		wall.hide()
 	walls[current_wall_index].show()
+	
+	selected_scene = walls[current_wall_index]
+	total_number_hover_info = selected_scene.get_node("HoverInfos").get_child_count()
+		
+func _input(event):
+	if event is InputEventKey:
+		if event.scancode == KEY_TAB and event.pressed:
+			selected_hover_info = selected_scene.get_node("HoverInfos").get_child(index_hover_info)
+			if index_hover_info == (total_number_hover_info-1):
+				index_hover_info = 0
+			else: 
+				index_hover_info += 1
+			if selected_hover_info.audio:
+				AudioPlayer.stop_all_audios_bus("MenuSpeech")
+				AudioPlayer.play_one_shot(selected_hover_info.audio, "MenuSpeech") 
+
 
 func set_current_wall(wall_index):
 	
@@ -26,5 +50,9 @@ func set_current_wall(wall_index):
 		wall_index = 0 # Vai para a primeira wall
 	
 	current_wall_index = wall_index # Muda a wall atual
+
+	selected_scene = walls[current_wall_index]
+	total_number_hover_info = selected_scene.get_node("HoverInfos").get_child_count()
+
 	
 	walls[current_wall_index].show() # Mostra a wall atual
